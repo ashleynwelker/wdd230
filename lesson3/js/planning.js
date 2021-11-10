8bcd2b4ce2ce339dc743e19929d7c550a3d670a5
ashleynwelker <ray13008@byui.edu>
2021-11-09 17:19:33 -0700

    Lesson 3 fixes

--- lesson3/js/planning.js
+++ lesson3/js/planning.js
@@ -1,6 +1,6 @@
 function dates() {
-    var lastModified = new Date(document.lastModified);
-    var currentYear = new Date();
+    let lastModified = new Date(document.lastModified);
+    let currentYear = new Date();
     document.getElementById("last-modified").innerHTML = lastModified.toLocaleString();
     document.getElementById("current-year").innerHTML = currentYear.getFullYear();
 }
