120068002c41a61c1893f612bde5bb589adff1e7
ashleynwelker <ray13008@byui.edu>
2021-11-22 15:28:17 -0700

    Lesson six fixes

--- lesson6/js/preston.js
+++ lesson6/js/preston.js
@@ -1,5 +1,5 @@
-let currentDate = new Date();
-
+function dates() {
+    let currentYear = new Date();
     let lastModified = new Date(document.lastModified);
     let options = {
         weekday: "long",
@@ -8,18 +8,26 @@ let currentDate = new Date();
         day: "numeric"
     }; 
     document.getElementById("last-modified").innerHTML = lastModified.toLocaleDateString("en", options);
-    document.getElementById("current-year").innerHTML = currentDate.getFullYear();
+    document.getElementById("current-year").innerHTML = currentYear.getFullYear();
+}
 
-if (currentDate.getDay() == 5) {
+dates();
+
+if (new Date().getDay() == 5) {
     document.getElementById("banner").style.display = "block";
 }
 
+let hamburger = document.querySelector(".icon");
+hamburger.addEventListener("click", toggleMenu);
+
+let list = document.querySelector(".menu");
 function toggleMenu() {
-    var list = document.getElementById("menu");
-    if (list.style.display === "block") {
-        list.style.display = "none";
-    } 
-    else {
-        list.style.display = "block";
-    }
+    console.log(list);
+    list.classList.toggle("openMenu");
 }
+
+window.onresize = () => {
+    if (window.innerWidth > 760) {
+        list.classList.remove("openMenu");
+    }
+} 
